4664159023240c2c819f564dd6ff12712ace3d5d
Michael Robinson <mike@pagesofinterest.net>
2024-04-28 12:08:55 +0800

    Remove chinese parenthesis

--- src/anki/note-text-filter.ts
+++ src/anki/note-text-filter.ts
@@ -4,6 +4,7 @@ export const filterNoteContent = (input: string): string => {
     return input // https://chat.openai.com/chat/7e66101d-3410-4570-9b5d-2b65aac583cf
         .replace(/\([^)]*\)/g, '') // remove content in parentheses
         .replace(/<\/?[^>]+(>|$)/g, '') // remove HTML tags
+        .replace(/[\u4e00-\u9fa5]（[\u4e00-\u9fa5]+）/g, '') // Remove chinese parentheses
         .trim();
 };
 
